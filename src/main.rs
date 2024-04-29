596e1a8695c2f1ebe7463505b1521b6c05132733
Sébastien VIAL <sebastien@shyrogan.fr>
2024-04-29 08:56:44 +0200

    tests: include tests

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,18 @@ async fn main() -> color_eyre::Result<()> {
         .with(fmt::layer())
         .with(EnvFilter::from_default_env());
 
+    let listener = TcpListener::bind(format!(
+        "{}:{}",
+        option_env!("SERVER_IP").unwrap_or("0.0.0.0"),
+        option_env!("SERVER_PORT").unwrap_or("3000")
+    ))
+    .await?;
+
+    axum::serve(listener, app().await?).await?;
+    Ok(())
+}
+
+async fn app() -> color_eyre::Result<Router> {
     // Base de donnée
     let mut options = ClientOptions::parse(std::env::var("MONGO")?).await?;
     options.app_name = Some("Journai".to_string());
@@ -55,13 +67,44 @@ async fn main() -> color_eyre::Result<()> {
         .layer(Extension(entries_collection))
         .layer(cors);
 
-    let listener = TcpListener::bind(format!(
-        "{}:{}",
-        option_env!("SERVER_IP").unwrap_or("0.0.0.0"),
-        option_env!("SERVER_PORT").unwrap_or("3000")
-    ))
-    .await?;
+    Ok(app)
+}
 
-    axum::serve(listener, app).await?;
-    Ok(())
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt; // for `collect`
+    use tower::ServiceExt; // for `call`, `oneshot`, and `ready` // for `collect`
+
+    use crate::app;
+
+    #[tokio::test]
+    async fn get_entries() {
+        dotenvy::dotenv().expect("Requires environment variables");
+        let app = app().await.expect("Unable to start router");
+
+        // `Router` implements `tower::Service<Request<Body>>` so we can
+        // call it like any tower service, no need to run an HTTP server.
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = String::from_utf8(
+            response
+                .into_body()
+                .collect()
+                .await
+                .unwrap()
+                .to_bytes()
+                .to_vec(),
+        )
+        .expect("We expect to have a body");
+        assert!(!body.is_empty())
+    }
 }
